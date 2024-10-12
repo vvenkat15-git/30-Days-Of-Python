b1149264296432a12fef1fecb57443dfc0c201c7
VeNkat <72157165+vvenkat15-git@users.noreply.github.com>
2024-10-12 12:04:57 +0530

    Create Hello_world.py

---
+++ Day_01/Hello_world.py
@@ -0,0 +1,29 @@
+#Introduction
+# Day 1 - 30DAYSOFPYTHON CHALLENGE
+
+#code
+
+#pirntHello World
+print("Hello World")
+
+print(3+2)   #Addition(+)
+print(3-2)   #Subtraction(-)
+print(3*2)   #Multiplication(*)
+print(3/2)   #Division(/)
+print(3**2)  #Exponential(**) 
+print(3%2)   #Modulus(%)
+print(3//2)  #Floor Division Operator()
+
+
+#Checkinf with data types
+
+print(type(10))                 #int
+print(type(3.14))               # Float
+print(type(1+ 3j))              # Complex
+print(type("Venkat"))           # String
+print(type([1,2,3]))            # List 
+print(type({'name':"venkat"}))  #Dictionary 
+print(type({9.8,3.14,2.7}))     #set
+print(type(9.8, 3.14, 2.7))     #Tuple 
+print(type(3==3))               #Bool
+print(type(3 >= 3))             #Bool
